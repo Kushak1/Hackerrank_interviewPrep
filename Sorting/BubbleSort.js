51df3dc961db257574322188b55be06d0de67797
Kushak1 <42020387+Kushak1@users.noreply.github.com>
2020-06-03 01:36:23 +0400

    Update BubbleSort.js

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,14 +1,13 @@
 function countSwaps(a) {
 
-for (let i = 0; i < a.length; i++) {
+for (let i = 0; i < a.length - 1; i++) {
     
-    for (let j = 0; j < a.length - 1; j++) {
+    for (let j = 0; j < a.length - i; j++) {
 
         if (a[j] > a[j + 1]) {
             let tmp = a[j];
             a[j] = a[j + 1];
             a[j + 1] = tmp;
-            count++;
         }
     }
     
