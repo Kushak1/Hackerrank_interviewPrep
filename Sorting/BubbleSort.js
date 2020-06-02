ac6bf2f6ce5affc83043570dc4842b8ec1532ae9
Kushak1 <42020387+Kushak1@users.noreply.github.com>
2020-06-03 00:59:29 +0400

    Update BubbleSort.js

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -9,7 +9,7 @@ for (let i = 0; i < a.length; i++) {
             let tmp = a[j];
             a[j] = a[j + 1];
             a[j + 1] = tmp;
-            count++
+            count++;
         }
     }
     
