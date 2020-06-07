c2f1c8b65235fde2eeb7e275d7581fed76840c89
Kushak1 <42020387+Kushak1@users.noreply.github.com>
2020-06-07 20:31:26 +0400

    Create AlternatingCharacters.js

---
+++ StringManipulation/AlternatingCharacters.js
@@ -0,0 +1,13 @@
+function alternatingCharacters(s) {
+
+s = s.split("");
+let count = 0;
+
+for(let i = 0; i < s.length; i++){
+    if(s[i] == s[i+1]){
+        count++;
+    }
+
+  }
+return count;
+}
