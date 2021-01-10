c1847aa73c6e647330c32ca1f7d0308f198545d1
Kushak1 <42020387+Kushak1@users.noreply.github.com>
2021-01-10 04:23:59 +0400

    Create MinimumSwaps2.php

---
+++ Arrays/MinimumSwaps2.php
@@ -0,0 +1,19 @@
+function minimumSwaps($arr) {
+    
+$swaps = 0;
+
+for($i=0;$i<count($arr)-1;$i++){
+        
+        $current = $arr[$i];
+        $pos = $arr[$current-1];
+        while($current!==$pos){
+          
+        $arr[$i] = $pos;
+        $arr[$current-1] = $current;
+        $current = $pos;
+        $pos=$arr[$current-1];
+        $swaps++;
+                
+        }  
+          
+}
